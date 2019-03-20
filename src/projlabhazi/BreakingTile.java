0876f96b7acefe35cdf79f9ba5f4c9741d84f101
Mark Toth <tothmarkandor99@freemail.hu>
2019-03-20 14:46:51 +0100

    BreakingTile draft done

--- src/projlabhazi/BreakingTile.java
+++ src/projlabhazi/BreakingTile.java
@@ -5,16 +5,27 @@ public class BreakingTile extends Tile {
 	
 	@Override
 	public boolean receive(Orangutan o) {
-
+		if (getObject() == null)
+			return false;
+		setObject(o);
+		crack();
+		return true;
 	}
 
 	@Override
 	public boolean receive(Panda p) {
-
+		if (getObject() == null)
+			return false;
+		setObject(p);
+		crack();
+		return true;
 	}
 
+
 	public void crack() {
-		
+		if (life == 0) {
+			Break((Character)getObject());
+		}
 	}
 	
 	public void Break(Character c) {
--- src/projlabhazi/Tile.java
+++ src/projlabhazi/Tile.java
@@ -6,7 +6,7 @@ public class Tile implements Receiver {
 	
 	@Override
 	public boolean receive(Orangutan o) {
-		if (object != null)
+		if (object == null)
 			return false;
 		object = o;
 		return true;
@@ -14,14 +14,24 @@ public class Tile implements Receiver {
 
 	@Override
 	public boolean receive(Panda p) {
-		if (object != null)
+		if (object == null)
 			return false;
 		object = p;
 		return true;
 	}
 
-	public void removeNeighbour(Tile tile) {
-		//TODO: ezt lehet nem így kéne
+	public void removeNeighbour(Tile tile) { //TODO: ezt lehet nem így kéne
+		int i = 0;
+		while (i < neighbours.length && neighbours[i] != tile) {
+			i++;
+		}
+		if (i != neighbours.length) { //Found 
+			Tile[] newNeighbours = new Tile[neighbours.length - 1];
+			for (int j = 0; j < newNeighbours.length; j++) { //Copy except the parameter tile
+				newNeighbours[j] = neighbours[j + j >= i ? 1 : 0];
+			}
+			neighbours = newNeighbours;
+		}
 	}
 	
 	public boolean put(Character c, int i) {
